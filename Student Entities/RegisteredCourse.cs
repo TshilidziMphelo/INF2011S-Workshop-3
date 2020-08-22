7610c58a60b17327d98934185f8021a8d2249f25
Tshilidzi Mphelo <temphelo95@gmail.com>
2020-08-22 14:06:47 +0200

    Creating RegisteredCourse, it's a child class of Course. Also adding its fields

---
+++ Student Entities/RegisteredCourse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentInfo_system.Student_Entities
+{
+    public class RegisteredCourse : Course
+    {
+        #region RegisteredCourse Fields
+        private int studentId;
+        private int percent;
+        private string symbol;
+        #endregion
+    }
+}
